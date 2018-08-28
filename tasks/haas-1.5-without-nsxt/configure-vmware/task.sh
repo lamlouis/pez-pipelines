dfd738f5927c6666e30bdb43fdccee3cf460ec38
Louis Lam <llam@pivotal.io>
2018-08-28 16:14:17 -0700

    disabled vswitch creation

--- tasks/haas-1.5-without-nsxt/configure-vmware/task.sh
+++ tasks/haas-1.5-without-nsxt/configure-vmware/task.sh
@@ -3,20 +3,20 @@
 
 # create vSwitch1
 
-/opt/pyvmomi/add_vswitch_to_host.py \
-  --user $HAAS_SLOT_VCENTER_USR \
-  --password $HAAS_SLOT_VCENTER_PWD \
-  --host $HAAS_SLOT_VCENTER_HOST \
-  --vswitch vSwitch1 \
-  --uplink1 vmnic4 \
-  --uplink2 vmnic5
+#/opt/pyvmomi/add_vswitch_to_host.py \
+#  --user $HAAS_SLOT_VCENTER_USR \
+#  --password $HAAS_SLOT_VCENTER_PWD \
+#  --host $HAAS_SLOT_VCENTER_HOST \
+#  --vswitch vSwitch1 \
+#  --uplink1 vmnic4 \
+#  --uplink2 vmnic5
 
 # add new portgroup
 
-/opt/pyvmomi/add_portgroup_to_vswitch.py \
-  --user $HAAS_SLOT_VCENTER_USR \
-  --password $HAAS_SLOT_VCENTER_PWD \
-  --host $HAAS_SLOT_VCENTER_HOST \
-  --portgroup PAS\
-  --vlanid $SECOND_VLAN \
-  --vswitch vSwitch1
+#/opt/pyvmomi/add_portgroup_to_vswitch.py \
+#  --user $HAAS_SLOT_VCENTER_USR \
+#  --password $HAAS_SLOT_VCENTER_PWD \
+#  --host $HAAS_SLOT_VCENTER_HOST \
+# --portgroup PAS\
+#  --vlanid $SECOND_VLAN \
+#  --vswitch vSwitch1
